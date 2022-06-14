629643d665862531dc83972db8ba894d1f459de5
Akadoshin <anderson344741@gmail.com>
2022-06-14 18:49:58 -0500

    feat: uwp-2790 new deploy bucket implementation
    create a script to have a better implementation for the deploy_bucket action
    
    BREAKING CHANGE: deploy_bucket@v1 is not compatible with deploy_bucket@main

--- deploy_bucket/README.md
+++ deploy_bucket/README.md
@@ -12,12 +12,17 @@ jobs:
   test:
     runs-on: ubuntu-latest
     steps:
-      - uses: MiloCreditPlatform/gh-actions/deploy_bucket@main
+      - uses: MiloCreditPlatform/gh-actions/deploy_bucket@v1
         with:
           AWS_ACCESS_KEY_ID: ${{ secrets... }}
           AWS_SECRET_ACCESS_KEY: ${{ secrets... }}
           INVALIDATE_DIST: ${{ secrets... }} # invalidate distribution
           INVALIDATE_PATHS: '/something*' # default '/*'
-          delete_bucket: "aws s3 rm ... "
-          sync_bucket: "aws s3 sync ..."
+          remove_bucket: "something.milocredit.com folder/"
+          sync_bucket: "something.milocredit.com folder/"
+
+          # --------- OR ---------
+
+          remove_bucket: "('something1.milocredit.com folder1/' 'something2.milocredit.com folder2/')"
+          sync_bucket: "('something1.milocredit.com folder1/' 'something2.milocredit.com folder2/')"
 ```
---
+++ deploy_bucket/deployBucket.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Name: deployBucket.sh
+# Purpose: Deploys a bucket to AWS S3 bucket with the name of the bucket passed as an argument.
+# -----------------------------------------------------------------------------------
+
+declare input_bucket_array=("$1")
+declare input_command=("$2")
+
+declare -a bucket_array="${input_bucket_array[@]}"
+
+# Sync the bucket to S3
+if [ $input_command == "sync" ]; then
+    for bucket in "${bucket_array[@]}"
+    do
+        bucket_values=($bucket)
+        echo "Syncing bucket: ${bucket_values[0]} to: ${bucket_values[1]}"
+        aws s3 sync s3://${bucket_values[0]} ${bucket_values[1]}
+    done
+# Remove the bucket from AWS S3
+elif [ $input_command == "remove" ]; then
+    for bucket in "${input_bucket_array[@]}"
+    do
+        bucket_values=($bucket)
+        echo "Removing bucket: ${bucket_values[0]}"
+        # Validate the bucket exists
+        if [ aws s3 ls s3://${bucket_values[0]} ]; then
+            echo "Bucket exists, removing bucket: ${bucket_values[0]}"
+            aws s3 rm s3://${bucket_values[0]} --recursive
+        else
+            echo "Bucket does not exist, skipping..."
+        fi
+    done
+else
+    echo "invalid command"
+fi
