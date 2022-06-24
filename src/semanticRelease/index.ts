c8623ca92124d7841560162a6279ba69bb4db0b6
Akadoshin <anderson344741@gmail.com>
2022-06-24 18:07:51 -0500

    feat(comment_pr): init comment pr action

--- src/semanticRelease/index.ts
+++ src/semanticRelease/index.ts
@@ -3,25 +3,28 @@ import * as core from '@actions/core';
 // import * as github from '@actions/github';
 import * as exec from '@actions/exec';
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  // const time = new Date().toTimeString();
-  // core.setOutput('time', time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2);
-  // console.log(`The event payload: ${payload}`);
+const app = async () => {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    // const time = new Date().toTimeString();
+    // core.setOutput('time', time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2);
+    // console.log(`The event payload: ${payload}`);
 
-  exec.exec('npm', [
-    'install',
-    '--location=global',
-    'semantic-release',
-    ' --silent',
-  ]);
-  exec.exec('npx', ['semantic-release']);
+    await exec.exec('npm', [
+      'install',
+      '--location=global',
+      'semantic-release',
+    ]);
+    await exec.exec('npx', ['semantic-release']);
 
-  console.log(JSON.stringify(process.env, undefined, 2));
-} catch (error) {
-  core.setFailed(error as Error);
-}
+    console.log(JSON.stringify(process.env, undefined, 2));
+  } catch (error) {
+    core.setFailed(error as Error);
+  }
+};
+
+app();
