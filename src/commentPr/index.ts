42311cb4fc506cb660d72095a8befc2e8177d945
Akadoshin <anderson344741@gmail.com>
2022-06-24 14:02:31 -0500

    feat(comment_pr): init comment pr action

--- src/commentPr/index.ts
+++ src/commentPr/index.ts
@@ -1,9 +1,11 @@
 import * as core from '@actions/core';
-// import * as github from '@actions/github';
+import * as github from '@actions/github';
 
 try {
-  const message = core.getInput('message');
+  const message: string = core.getInput('message');
   console.log('message', message);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error as Error);
 }
