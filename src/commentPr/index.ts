fb1544b9b35665d5eae8a339d7e68de4f68cf74d
Akadoshin <anderson344741@gmail.com>
2022-06-24 14:10:29 -0500

    feat(comment_pr): init comment pr action

--- src/commentPr/index.ts
+++ src/commentPr/index.ts
@@ -2,9 +2,19 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 try {
+  const { context } = github;
   const message: string = core.getInput('message');
-  console.log('message', message);
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  const pullRequest: number = context.payload.pull_request?.number || 0;
+
+  /** if the pull request number doesn't exits, it'll returns an error */
+  if (pullRequest === 0) {
+    const error = 'pull request number is not valid';
+    core.setFailed(error);
+    throw new Error(error);
+  }
+
+  console.log('message', message, pullRequest);
+  const payload = JSON.stringify(context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error as Error);
