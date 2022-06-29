0e14e567f4f201d64458af703d99542ac39a485c
Akadoshin <anderson344741@gmail.com>
2022-06-29 11:40:19 -0500

    chore(workflow): semantic release update env

--- src/semanticRelease/index.ts
+++ src/semanticRelease/index.ts
@@ -3,13 +3,17 @@ import * as core from '@actions/core';
 // import * as github from '@actions/github';
 import * as exec from '@actions/exec';
 
+import path from 'node:path';
+import fs from 'node:fs';
+
 const app = async () => {
   try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
     console.log(JSON.stringify(process.env, undefined, 2));
-    await exec.exec('ls');
+    const releaserc = fs.readFileSync(path.join(__dirname, '.releaserc'));
+    console.log(releaserc);
     // const time = new Date().toTimeString();
     // core.setOutput('time', time);
     // Get the JSON webhook payload for the event that triggered the workflow
