ee69de8651e8b2f3b459b5b15dc6588ebad4ade0
Akadoshin <anderson344741@gmail.com>
2022-06-29 15:27:04 -0500

    chore(workflow): semantic release update env

--- src/semanticRelease/index.ts
+++ src/semanticRelease/index.ts
@@ -10,7 +10,11 @@ const app = async () => {
   try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
-    /** repository workspace where the action is used */
+
+    // semantic release config file name
+    const releasercFile = '.releaserc';
+
+    // repository workspace where the action is used
     const workSPace = process.env.GITHUB_WORKSPACE || '';
 
     console.log(`Hello ${nameToGreet}!`);
@@ -19,10 +23,17 @@ const app = async () => {
       workSPace,
       __dirname,
     );
-    const releaserc = fs.readFileSync(path.join(workSPace, '.releaserc'), {
+    const releaserc = fs.readFileSync(path.join(workSPace, releasercFile), {
       encoding: 'utf8',
     });
+    // const time = new Date().toTimeString();
+    // core.setOutput('time', time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2);
+    // console.log(`The event payload: ${payload}`);
+
     if (releaserc) {
+      await exec.exec('npm', ['install', 'semantic-release']);
       const jsonReleaserc = JSON.parse(releaserc);
 
       if (jsonReleaserc.plugins) {
@@ -32,9 +43,11 @@ const app = async () => {
           '@semantic-release/npm',
           '@semantic-release/github',
         ]);
-        const installPlugins = jsonReleaserc.plugins
-          .flat()
-          .filter((plugin: any) => typeof plugin === 'string' && !defaultPlugins.has(plugin));
+        const installPlugins = jsonReleaserc.plugins.flat().filter(
+          (plugin: any) =>
+            // eslint-disable-next-line implicit-arrow-linebreak
+            typeof plugin === 'string' && !defaultPlugins.has(plugin),
+        );
 
         console.log(installPlugins);
 
@@ -42,16 +55,12 @@ const app = async () => {
           await exec.exec('npm', ['install', plugin]);
         });
       }
-    }
-    // const time = new Date().toTimeString();
-    // core.setOutput('time', time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2);
-    // console.log(`The event payload: ${payload}`);
 
-    await exec.exec('npm', ['install', 'semantic-release']);
-    await exec.exec('npx', ['semantic-release']);
-    await exec.exec('git', ['status']);
+      await exec.exec('npx', ['semantic-release']);
+      await exec.exec('git', ['status']);
+    } else {
+      core.setFailed(`file config: ${releasercFile} NOT found`);
+    }
   } catch (error) {
     core.setFailed(error as Error);
   }
